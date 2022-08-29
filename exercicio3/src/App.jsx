1b36a92bc90ca8eaa18b2954aba71b930b7700d2
Jakeline-xx <jakeline.santana.rocha@gmail.com>
2022-08-29 00:33:58 -0300

    finalizado

--- exercicio3/src/App.jsx
+++ exercicio3/src/App.jsx
@@ -5,6 +5,8 @@ import Carteirinha from "./components/carteirinha/Carteirinha";
 import FotoUm from "./imgs/carteirinhaUm.png";
 import FotoDois from "./imgs/carteirinhaDois.png";
 import FotoTres from "./imgs/carteirinhaTres.png";
+import FotoQuatro from "./imgs/carteirinhaQuatro.png";
+import { useState } from "react";
 
 export default function App() {
   const listaCarros = [
@@ -14,7 +16,7 @@ export default function App() {
     "Fiat Mobi",
     "Civic",
     "Celta",
-    "Mustanh",
+    "Mustang",
     "Onix",
     "Fusca",
     "Brasilia",
@@ -36,6 +38,13 @@ export default function App() {
 
   const carteirinhaTres = ["Lucas Santana da Rocha", "RM96267", "ADS", "1TDSR"];
 
+  const [nome, setNome] = useState("");
+  const [rm, setRm] = useState("");
+  const [curso, setCurso] = useState("");
+  const [turma, setTurma] = useState("");
+
+  const carteirinhaQuatro = [nome, rm, curso, turma];
+
   return (
     <>
       <Semaforo />
@@ -43,6 +52,45 @@ export default function App() {
       <Carteirinha fotoProps={FotoUm} carteirinhaProps={carteirinhaUm} />
       <Carteirinha fotoProps={FotoDois} carteirinhaProps={carteirinhaDois} />
       <Carteirinha fotoProps={FotoTres} carteirinhaProps={carteirinhaTres} />
+      <Carteirinha
+        fotoProps={FotoQuatro}
+        carteirinhaProps={carteirinhaQuatro}
+      />
+      <ul>
+        <li>
+          {" "}
+          <input
+            type="text"
+            name="nome"
+            onChange={(x) => setNome(x.target.value)}
+            placeholder="Insira o nome"
+          ></input>
+        </li>
+        <li>
+          <input
+            type="text"
+            name="rm"
+            onChange={(x) => setRm(x.target.value)}
+            placeholder="Insira o rm"
+          ></input>
+        </li>
+        <li>
+          <input
+            type="text"
+            name="curso"
+            onChange={(x) => setCurso(x.target.value)}
+            placeholder="Insira o curso"
+          ></input>
+        </li>
+        <li>
+          <input
+            type="text"
+            name="turma"
+            onChange={(x) => setTurma(x.target.value)}
+            placeholder="Insira a turma"
+          ></input>
+        </li>
+      </ul>
     </>
   );
 }
